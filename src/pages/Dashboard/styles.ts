9e95034deefd68c1100b558530ea7d28c32968b2
Lubnnia <lubnnia@gmail.com>
2022-06-20 23:32:35 -0300

    Finish form css style of Dashboard

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 // Template literals
 export const Title = styled.h1`
@@ -22,6 +23,11 @@ export const Form = styled.form`
     padding: 0 24px;
     border: 0;
     border-radius: 5px 0 0 5px;
+    color: #3a3a3a;
+
+    &:placeholder {
+      color: #a8a8b3;
+    }
   }
 
   button {
@@ -29,5 +35,13 @@ export const Form = styled.form`
     height: 70px;
     background: #04d361;
     border-radius: 0 5px 5px 0;
+    border: 0;
+    color: #fff;
+    font-weight: bold;
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: ${shade(0.2, '#04d361')};
+    }
   }
 `;
