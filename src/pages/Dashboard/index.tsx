edb83148f909b015ee5fae326a2466721fde9ca3
Lubnnia <lubnnia@gmail.com>
2022-06-22 12:54:34 -0300

    Finish style css list of Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logoImg from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,45 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="http://teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/74938862?v=4"
+            alt="Lubnnia Morais"
+          />
+          <div>
+            <strong>primeiro-projeto-react</strong>
+            <p>Projeto React do Nível 03 - GoStack</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="http://teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/74938862?v=4"
+            alt="Lubnnia Morais"
+          />
+          <div>
+            <strong>primeiro-projeto-react</strong>
+            <p>Projeto React do Nível 03 - GoStack</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+        <a href="http://teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/74938862?v=4"
+            alt="Lubnnia Morais"
+          />
+          <div>
+            <strong>primeiro-projeto-react</strong>
+            <p>Projeto React do Nível 03 - GoStack</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
